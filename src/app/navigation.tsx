62be1847fd2e7cfbdc46c8b6e20b5b15dc6dcd8f
= <jul204040204625@gmail.com>
2024-02-12 14:02:34 -0500

    nav highlighting

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./navigation.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+	const path = usePathname();
 	return (
 		<nav className={styles.nav}>
-			<Link href={"/"}>Home</Link>
-			<Link href={"/request"}>Request Us</Link>
-			<Link href={"/join"}>Become a DJ</Link>
-			<Link href={"/variety"}>Variety Hour</Link>
-			<Link href={"/gallery"}>Gallery</Link>
-			<Link href={"/staff"}>Staff</Link>
+			<Link href={"/"} className={(path == '/') ? styles.selected: undefined}>Home</Link>
+			<Link href={"/request"} className={(path == '/request/') ? styles.selected: undefined}>Request Us</Link>
+			<Link href={"/join"} className={(path == '/join/') ? styles.selected: undefined}>Become a DJ</Link>
+			<Link href={"/variety"} className={(path == '/variety/') ? styles.selected: undefined}>Variety Hour</Link>
+			<Link href={"/gallery"} className={(path == '/gallery/') ? styles.selected: undefined}>Gallery</Link>
+			<Link href={"/staff"} className={(path == '/staff/') ? styles.selected: undefined}>Staff</Link>
 		</nav>
 	);
 }
