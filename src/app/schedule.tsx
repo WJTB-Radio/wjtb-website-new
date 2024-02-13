a4d0f53bc143f17636715b69060fda3ef9a9b26d
= <jul204040204625@gmail.com>
2024-02-12 21:43:41 -0500

    update schedule when it should change

--- src/app/schedule.tsx
+++ src/app/schedule.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { DayOfWeek, formatDay, formatTimes, getNYCTime, getNYCWeekday, getNYCWeekdayString, getWeekdayString } from "./utils/time";
 import styles from "./schedule.module.scss";
 import useSWR from "swr";
@@ -10,12 +10,21 @@ type Show = {name: string, desc: string, hosts: string, poster: string, start_ti
 type DaySchedule = {day: DayOfWeek, shows: Array<Show>};
 
 export default function Schedule() {
+	const [, forceUpdate] = useReducer(x => x + 1, 0);
+	
+	let playingShow: Show | undefined;
+	let nextShow: Show | undefined;
+	let nextShowDay: number | undefined;
+
 	let computed: Array<React.JSX.Element> = [];
 	const currentDay = getNYCWeekday();
 	const time = getNYCTime();
 	for(let i = 0; i < 7; i++) {
 		let d = (currentDay+i)%5;
-		let {data, error}: {data: DaySchedule | undefined, error: boolean | undefined} = useSWR(`https://raw.githubusercontent.com/WJTB-Radio/ShowData/master/${getWeekdayString(d)}.json`, jsonFetcher);
+		// this for loop always runs the same number of times, so we can disable the linter rule
+		let {data, error}: {data: DaySchedule | undefined, error: boolean | undefined} =
+				// eslint-disable-next-line react-hooks/rules-of-hooks
+				useSWR(`https://raw.githubusercontent.com/WJTB-Radio/ShowData/master/${getWeekdayString(d)}.json`, jsonFetcher);
 		if(!data || error) {
 			continue;
 		}
@@ -24,15 +33,30 @@ export default function Schedule() {
 			data.shows = data.shows.filter((show) => show.end_time > time);
 		}
 		const dayString = data.day;
-		computed.push(
-			<tbody>
-				<tr className={styles.day}>
-					<th>{formatDay(data.day)}</th>
-				</tr>
-			</tbody>
-		);
+		if(data.shows.length != 0) {
+			computed.push(
+				<tbody key={i+"day"}>
+					<tr className={styles.day}>
+						<th>{formatDay(data.day)}</th>
+					</tr>
+				</tbody>
+			);
+		}
+		if(i == 0) {
+			playingShow = data.shows.find((show) => {
+				return show.start_time <= time && show.end_time > time;
+			});
+		}
+		if(!nextShow) {
+			nextShow = data.shows.find((show) => {
+				return show.start_time >= time || d > currentDay;
+			});
+			if(nextShow) {
+				nextShowDay = d;
+			}
+		}
 		computed = computed.concat(data.shows.map(show =>
-			<tbody key={show.name+dayString} className={`${styles.show_container} 
+			<tbody key={show.name+i} className={`${styles.show_container} 
 					${(show.start_time <= time && show.end_time > time && i == 0 && show.is_running) && styles.playing}
 					${!show.is_running && styles.cancelled}`}>
 				<tr className={`${styles.show}`}>
@@ -46,6 +70,24 @@ export default function Schedule() {
 			</tbody>
 		));
 	}
+
+	// update when the playing show should change
+	useEffect(() => {
+		let updateTime: number | undefined;
+		if(playingShow) {
+			updateTime = (playingShow.end_time-time)*1000+1000;
+		} else if(nextShow && nextShowDay) {
+			updateTime = (nextShow.start_time-time+(nextShowDay-currentDay)*(24*60*60))*1000+1000;
+		}
+		if(updateTime) {
+			const timeout = window.setTimeout(() => {
+				forceUpdate();
+			}, updateTime);
+			return () => {
+				window.clearTimeout(timeout);
+			};
+		}
+	});
 
 	return (
 		<div className={styles.container} tabIndex={-1}>
