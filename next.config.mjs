2679992d6ffccb5365042b1537e03e9c21a5750c
julia <alara@alarabread.fun>
2024-09-05 02:23:24 -0400

    transpile packages

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,26 @@ const nextConfig = {
 	trailingSlash: true,
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+	transpilePackages: [
+		"@radix-ui/react-collapsible",
+		"@radix-ui/react-slot",
+		"class-variance-authority",
+		"clsx",
+		"core-js",
+		"embla-carousel-autoplay",
+		"embla-carousel-react",
+		"hls.js",
+		"lucide-react",
+		"next",
+		"node-sass",
+		"react",
+		"react-dom",
+		"react-use-cookie",
+		"sass",
+		"swr",
+		"tailwind-merge",
+		"tailwindcss-animate",
+	],
 };
 
 export default nextConfig;
