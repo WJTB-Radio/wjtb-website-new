278ba67bc3a034a468a5714ba36af714968c4e0c
julia <59781761+AlaraBread@users.noreply.github.com>
2025-03-04 20:00:02 -0500

    fix link target

--- src/app/utils/block_renderer.tsx
+++ src/app/utils/block_renderer.tsx
@@ -8,7 +8,20 @@ import Link from "next/link";
 import { ReactNode } from "react";
 
 function StrapiBlockLink(props: { url: string; children?: ReactNode }) {
-	return <Link href={props.url}>{props.children}</Link>;
+	return (
+		<Link
+			href={props.url}
+			target={
+				!props.url.startsWith("/") ||
+				props.url == "/discord" ||
+				props.url.startsWith("/docs")
+					? "_blank"
+					: undefined
+			}
+		>
+			{props.children}
+		</Link>
+	);
 }
 
 export default function StrapiBlocks({
